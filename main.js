14d9bea1a49a9620e070a2f31730edcd2a6530ad
G-Gifted <125829057+G-Gifted@users.noreply.github.com>
2023-09-05 22:28:46 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var overload = document.getElementById('overload');
+var icon = document.getElementById('play');
+
+function play(){
+  if (overload.paused) {
+    overload.play();
+    icon.src = '/pause.jpg';
+  } else {
+    overload.pause();
+    icon.src = '/play.jpg';
+  }
